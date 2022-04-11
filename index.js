cff7f33992c439577d149573a3b272e922b58748
coleenhuang <coleenhuang@gmail.com>
2022-04-11 14:37:22 -0700

    implemented evenlistener for grid on hover

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const grid = document.querySelector('.grid-container');
 
 function insertSquares () {
-    for (let i = 0; i<16; i++) {
+    for (let i = 0; i<256; i++) {
         let div = document.createElement('div')
         div.classList.add('square')
         grid.appendChild(div)
     }
+    onHover()
+}
+
+function onHover () {
+    grid.addEventListener('mouseover', function(event) {
+        if(event.target.className == 'square') {
+            event.target.style.backgroundColor = 'black';
+        }
+        
+    })
 }
 
 insertSquares();
